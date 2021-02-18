a2fe52379b0d62a4e9c7b239773d4b06e96e444c
Jimmy Xue <jimx@hey.com>
2021-02-18 09:59:00 -0500

    updating graphql with cors

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -20,36 +20,41 @@ const todos = {}
 let todoIndex = 0;
 // Provide resolver functions for your schema fields
 const resolvers = {
-    Query: {
-        todos: () => {
-            return Object.values(todos);
-        }
-    },
-    Mutation: {
-        addTodo: (_, { text }) => {
-            todoIndex++;
-            const id = `key-${todoIndex}`;
-            todos[id] = { id, text, done: false };
-            return todos[id];
-        },
-        updateTodoDone: (_, { id }) => {
-            todos[id].done = true;
-            return todos[id];
-        }
+  Query: {
+    todos: () => {
+      return Object.values(todos);
     }
+  },
+  Mutation: {
+    addTodo: (_, { text }) => {
+      todoIndex++;
+      const id = `key-${todoIndex}`;
+      todos[id] = { id, text, done: false };
+      return todos[id];
+    },
+    updateTodoDone: (_, { id }) => {
+      todos[id].done = true;
+      return todos[id];
+    }
+  }
 }
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 
-    // By default, the GraphQL Playground interface and GraphQL introspection
-    // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
-    //
-    // If you'd like to have GraphQL Playground and introspection enabled in production,
-    // the `playground` and `introspection` options must be set explicitly to `true`.
-    playground: true,
-    introspection: true,
+  // By default, the GraphQL Playground interface and GraphQL introspection
+  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+  //
+  // If you'd like to have GraphQL Playground and introspection enabled in production,
+  // the `playground` and `introspection` options must be set explicitly to `true`.
+  playground: true,
+  introspection: true,
 });
 
-exports.handler = server.createHandler();
+exports.handler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+);
